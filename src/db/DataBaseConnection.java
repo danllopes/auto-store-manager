cd3b40fa4d6181f7932cd55e4d1eea74f6aef9c7
“Daniel <danielanderson.lopes0@gmail.com>
2024-07-22 13:07:22 -0300

    feat: adiciona método que estabelece a conexão com o bd

---
+++ src/db/DataBaseConnection.java
@@ -0,0 +1,25 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+
+    private static Connection connection = null;
+
+    private final static String url = "jdbc:mysql://localhost:3306/vehicle_store_bd";
+    private final static String user = "root";
+    private final static String password = "1234";
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (SQLException ex) {
+                throw new DataBaseException("Error while establishing database connection: " + ex.getMessage());
+            }
+        }
+        return connection;
+    }
+}
